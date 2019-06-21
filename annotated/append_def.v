18da8d6bbcc8020e1b2286b6651adfeb1d33c729
Qinshi Wang <qinshiw@cs.princeton.edu>
2019-06-21 22:30:45 +0800

    uncurry annotate spec

--- annotated/append_def.v
+++ annotated/append_def.v
@@ -61,7 +61,93 @@ intros.
  apply I.
 Qed.
 
+Definition spec111 :=
+  (fun sh x y s1 s2 =>
+    (PROP(writable_share sh)
+     LOCAL (temp _x x; temp _y y)
+     SEP (listrep sh s1 x; listrep sh s2 y),
+     EX r: val,
+       PROP()
+       LOCAL(temp ret_temp r)
+       SEP (listrep sh (s1++s2) r)
+    )
+  ).
+
+Ltac move_let_inside v :=
+  lazymatch goal with
+  | v := let (a, b) := _ in _ |- _ =>
+    lazymatch goal with
+    | v := let (a, b) := ?p in fun x:?T => ?content |- _ =>
+      let temp := fresh "temp" in
+      refine (fun x => _);
+      pose (temp := (fun x:T => let (a, b) := p in content) x);
+      hnf in temp;
+      clear v;
+      rename temp into v;
+      move_let_inside v
+    | v := let (a, b) := ?p in (?pre, ?post) |- _ =>
+      exact (let (a, b) := p in pre, let (a, b) := p in post)
+    | _ => fail 0 v "is not recognized"
+    end
+  | _ => fail 0 v "must have form let (a, b) := p in _"
+  end.
+
+Import Coq.Program.Tactics.
+
+Ltac foo spec :=
+  let spec_name := fresh "spec" in
+  pose (spec_name := spec);
+  unfold spec in spec_name;
+  repeat
+    lazymatch goal with
+    | spec_name := fun x:?T1 => fun y:?T2 => ?spec |- _ =>
+      first [ignore (T2 : _) | fail 2 "funcspec cannot have dependent type"];
+      first [
+        let spec_name1 := fresh "spec" in
+        pose (spec_name1 := (fun p : (T1*T2) => let (x, y) := p in spec));
+        clear spec_name;
+        rename spec_name1 into spec_name;
+        refine (let spec_name1 :=
+          ltac:(
+            match goal with
+            | spec_name := ?spec |- _ =>
+            let spec := eval unfold spec_name in spec_name in
+            let p := fresh "p" in
+            intro p;
+            pose (spec_name1 := spec p);
+            hnf in spec_name1;
+            clear spec_name;
+            rename spec_name1 into spec_name;
+            move_let_inside spec_name;
+            exact spec_name
+            end
+          ) in _);
+        clear spec_name;
+        rename spec_name1 into spec_name;
+        cbv beta zeta in spec_name
+      | fail 2 "Unknown error: failed to uncurry funcspec"
+      ]
+    end;
+  exact spec_name(*;
+  lazymatch goal with
+  | spec_name := fun x => (?pre, ?post) |- _ =>
+    let pre_name := fresh "pre" in
+    let post_name := fresh "post" in
+    pose (pre_name := pre);
+    pose (post_name := post);
+    clear spec_name
+  | _ => fail 0 "bbb"
+  end*).
+
+Definition spec222 :=
+  ltac:(foo spec111).
+
+Definition spec333 :=
+  ltac:(let spec := eval cbv beta zeta delta [spec222] in spec222 in exact spec).
+
+
 Definition append_spec :=
+  ltac: (convert
  DECLARE _append
   WITH sh : share, x: val, y: val, s1: list val, s2: list val
   PRE [ _x OF (tptr t_struct_list) , _y OF (tptr t_struct_list)]
